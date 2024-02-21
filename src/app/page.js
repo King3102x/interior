6f4e70efa6b8db857222ba0e81e139af3e8e7517
King3102x <132570585+King3102x@users.noreply.github.com>
2024-02-21 10:41:46 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
+import React from 'react'
+
 import Hero from '@/components/Hero'
 import Footer from '@/components/Footer'
+import Carousel2 from '@/Components/Carousel2'
 
 
 export default function Home() {
   return (
     <>
     <Hero/>
+      <Carousel2/>
+      <Hero/>
       <Footer/>
       </>
   )
